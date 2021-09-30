f719ab46d3cdbdaeea1346ccab2408dbd876be91
Aurora Brune <akbrune@hotmail.com>
2021-09-30 16:59:11 -0500

    removed react logo, added image to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AppBar from './components/Header/index';
 import Footer from './components/Footer/index';
 import Home from './components/pages/home';
+import largeImage from './components/assets/chicago-large.jpg';
+import cellImage from './components/assets/chicago-small.jpg';
 
 import './App.css';
 
 function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const imageUrl = windowWidth >= 650 ? largeImage : cellImage;
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    }
+  }, []);
+
   return (
     <div>
-        <AppBar />
-        <Home />
-        <Footer />
+      <div className="App" style={{ backgroundImage: `url(${imageUrl})` }}>
+        <div>  
+          <AppBar />
+          <Home />
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 }
--- client/src/components/pages/home.js
+++ client/src/components/pages/home.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Main from "./main";
 import Map from "./map";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 
 function Home() {
+
  return(
   <Switch>
    <Route exact path="/" component={Main} /> 
