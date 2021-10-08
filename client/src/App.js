c428c6d6cd89acbdeb56d4fcefcb4fdfd82e65f1
Aurora Brune <akbrune@hotmail.com>
2021-10-08 16:14:18 -0500

    moved map code to map page

--- client/src/App.js
+++ client/src/App.js
@@ -4,36 +4,22 @@ import Footer from './components/Footer/index';
 import Home from './components/pages/home';
 import largeImage from './components/assets/chicago-large.jpg';
 import cellImage from './components/assets/chicago-small.jpg';
-import Modal from "./components/Modal";
-
 import './App.css';
 
 function App() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const imageUrl = windowWidth >= 650 ? largeImage : cellImage;
-
+  //sets the width for background image sized for responsive ui
   useEffect(() => {
     const handleWindowResize = () => {
       setWindowWidth(window.innerWidth);
     };
-
-    window.addEventListener('resize', handleWindowResize);
+     window.addEventListener('resize', handleWindowResize);
 
     return () => {
       window.removeEventListener('resize', handleWindowResize);
     }
   }, []);
-
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [disable, setDisable] = React.useState(false);
-
-  React.useEffect(() => {
-    if (modalIsOpen) {
-      setDisable(true);
-    } else {
-      setDisable(false)
-    }
-  }, [modalIsOpen]);
 
   return (
     <div>
@@ -42,8 +28,6 @@ function App() {
         <div>
           <AppBar />
           <Home />
-          <button id="openModBtn" disabled={disable} type="button" className="openModalBtn" onClick={() => { setModalIsOpen(true); setDisable(true) }} >Open Map</button>
-          {modalIsOpen && <Modal closeModal={setModalIsOpen} />}
           <Footer />
         </div>
       </div>
--- client/src/components/pages/map.js
+++ client/src/components/pages/map.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "../Modal";
+import Footer from "../Footer";
 
-const Map = () => (
- <h1>this is the map page</h1>
-)
+const Map = () => {
+ const [modalIsOpen, setModalIsOpen] = useState(false);
+ const [disable, setDisable] = React.useState(false);
+ //disables/enables modal button when modal is open/closed
+ React.useEffect(() => {
+  if (modalIsOpen) {
+   setDisable(true);
+  } else {
+   setDisable(false)
+  }
+ }, [modalIsOpen]);
+
+ if (modalIsOpen) { }
+ return (
+  <div>
+   <div className="modBtn">
+     <button id="openModBtn" disabled={disable} type="button" className="openModalBtn" onClick={() => { setModalIsOpen(true); setDisable(true) }} >Open Map</button>
+     {modalIsOpen && <Modal closeModal={setModalIsOpen} />}
+   </div>
+     <Footer />
+  </div>
+ );
+ }
 
 export default Map;
