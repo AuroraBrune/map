1fa6bf296feac56216972e64919ca9ff67eb2c14
Aurora Brune <akbrune@hotmail.com>
2021-10-08 16:18:25 -0500

    added intro paragraph to main page

--- client/src/components/pages/main.js
+++ client/src/components/pages/main.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./style.css";
 
-const Main = () => (
- <h1>main page</h1>
-)
+function Main() {
+
+return (
+ <h1 className="intro" >For all you world travelers, here is an app where you can add pins to the locations you've traveled and keep notes about those locations. Open the map, and enjoy!!!</h1>
+);
+}
 
 export default Main;
