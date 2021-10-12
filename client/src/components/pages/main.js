441e68e1d9376ca2e80748cc8d9e13521da08d7a
Aurora Brune <akbrune@hotmail.com>
2021-10-12 16:03:02 -0500

    test sentence on main page

--- client/src/components/pages/main.js
+++ client/src/components/pages/main.js
@@ -3,9 +3,12 @@ import "./style.css";
 
 function Main() {
 
-return (
- <h1 className="intro" >For all you world travelers, here is an app where you can add pins to the locations you've traveled and keep notes about those locations. Open the map, and enjoy!!!</h1>
-);
+ return (
+  <div>
+   <h1 className="intro" >For all you world travelers, here is an app where you can add pins to the locations you've traveled and keep notes about those locations. Open the map, and enjoy!!!</h1>
+   <h1>Hope this sentence gets sent.</h1>
+  </div>
+ );
 }
 
 export default Main;
