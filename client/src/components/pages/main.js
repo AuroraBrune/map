362b121ce6380fa8a1acce3156a8f5fdbd43569f
Aurora Brune <akbrune@hotmail.com>
2021-10-12 16:04:13 -0500

    another sentence to test

--- client/src/components/pages/main.js
+++ client/src/components/pages/main.js
@@ -7,6 +7,7 @@ function Main() {
   <div>
    <h1 className="intro" >For all you world travelers, here is an app where you can add pins to the locations you've traveled and keep notes about those locations. Open the map, and enjoy!!!</h1>
    <h1>Hope this sentence gets sent.</h1>
+   <h1> Maybe this will. </h1>
   </div>
  );
 }
