d33cc8f61a4eb8d5c4d01d542e560088a4ba722e
Aurora Brune <akbrune@hotmail.com>
2021-10-12 16:18:37 -0500

    another test after re-deploying

--- client/src/components/pages/main.js
+++ client/src/components/pages/main.js
@@ -6,6 +6,7 @@ function Main() {
  return (
   <div>
    <h1 className="intro" >For all you world travelers, here is an app where you can add pins to the locations you've traveled and keep notes about those locations. Open the map, and enjoy!!!</h1>
+   <h1>test</h1>
   </div>
  );
 }
