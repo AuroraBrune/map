57ebe28879d2a05d26c267cdf2a498f833181e54
Aurora Brune <akbrune@hotmail.com>
2021-10-13 16:36:03 -0500

    corrections to package.json files ghPages

--- client/src/components/pages/main.js
+++ client/src/components/pages/main.js
@@ -6,7 +6,6 @@ function Main() {
  return (
   <div>
    <h1 className="intro" >For all you world travelers, here is an app where you can add pins to the locations you've traveled and keep notes about those locations. Open the map, and enjoy!!!</h1>
-   <h1>test</h1>
   </div>
  );
 }
